a7395e9403e303c2973f2ac123935f56a38e033a
ahmedshh <ahmedshahab6n@gmail.com>
2024-01-26 10:48:42 +0100

    trial

--- main.py
+++ main.py
@@ -1,4 +1,42 @@
-print ("hello")
-print ("tiral123")
-print ("its me")
 
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    pub_year = db.Column(db.Integer, nullable=False)
+
+@app.before_first_request
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
+@app.route('/books')
+def books():
+    all_books = Book.query.all()
+    return render_template('books.html', books=all_books)
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        pub_year = request.form['pub_year']
+
+        new_book = Book(title=title, author=author, pub_year=pub_year)
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        return redirect(url_for('books'))
+
+    return render_template('add_book.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
